02791d54fced305edb1f6752a0b8fac4721a5ee2
UdaraJayawardena <udarajaywardena5@gmail.com>
2019-12-20 16:43:57 +0530

    cloud3

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.use(multerMid.single('file'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+
+app.use(cors({ credentials: true, origin: true }))
+
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.post('/uploads', async (req, res, next) => {
 
     try {
